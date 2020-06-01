e18ce78e2c986d2f070bb105ee740c43c4ec9e58
Vincent Hoang <vietnhoang7@gmail.com>
2020-06-01 16:01:32 -0700

    Initial commit

---
+++ Generics Challenge.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+struct CountedSet<Element: Hashable>: ExpressibleByArrayLiteral {
+    
+    typealias ArrayLiteralElement = Element
+    
+    private (set) var dict = [Element : Int]()
+    
+    var count: Int {
+        return dict.count
+    }
+    
+    mutating func insert(_ element: Element) {
+        if let _ = dict[element] {
+            dict[element]! += 1
+        } else {
+            dict[element]! = 0
+        }
+    }
+    
+    mutating func remove(_ element: Element) {
+        if let _ = dict[element] {
+            dict[element]! -= 1
+        } else {
+            dict.removeValue(forKey: element)
+        }
+    }
+    
+    subscript(_ member: Element) -> Int {
+        if let _ = dict[member] {
+            return dict[member]!
+        }
+        return 0
+    }
+    
+    init(arrayLiteral: Element...) {
+        self.init()
+    }
+}
+
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
+myCountedSet.remove(.iron) // 3
+myCountedSet.remove(.dwarvish) // 0
+myCountedSet.remove(.magic) // 0
